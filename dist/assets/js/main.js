e7df9083507f6e55a617484df2e79baca3f7e0d2
enes kırdemir <alptekinenes@gmail.com>
2021-04-20 16:41:23 +0300

    home page created

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
 $(document).ready(function () {
-  $('body').autoPadding({
-    source: $('.js-header')
+  $("body").autoPadding({
+    source: $(".js-header")
   }); //removeIf(production)
 
   console.log("document ready"); //endRemoveIf(production)
 });
 $(document).ready(function () {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,18 +1,19 @@
 "use strict";
 
-
-$(document).ready(function() {
-  $('body').autoPadding({
-    source: $('.js-header'),
+$(document).ready(function () {
+  $("body").autoPadding({
+    source: $(".js-header"),
   });
-    //removeIf(production)
-    console.log("document ready");
-    //endRemoveIf(production)
+  //removeIf(production)
+  console.log("document ready");
+  //endRemoveIf(production)
 });
 
 $(document).ready(function () {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 });
