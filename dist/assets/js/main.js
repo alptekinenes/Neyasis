2faceaa182e3b5cc06eb522913108758d9a11877
enes kırdemir <alptekinenes@gmail.com>
2021-04-20 12:19:36 +0300

    initial commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function () {
+  $('body').autoPadding({
+    source: $('.js-header')
+  }); //removeIf(production)
+
+  console.log("document ready"); //endRemoveIf(production)
+});
+$(document).ready(function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
